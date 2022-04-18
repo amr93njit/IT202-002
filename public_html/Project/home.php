c6587b78fb2799459fb763c7c1743404debb1f5b
BuildTools <unconfigured@null.spigotmc.org>
2022-04-18 01:06:32 -0400

    added scoreboards: profile page showing last 10 scores and functions to display top 10 weekly/monthly/lifetime

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,14 +1,17 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-<h1>Home</h1>
-<?php
-
-if (is_logged_in(true)) {
-    //comment this out if you don't want to see the session variables
-    error_log("Session data: " . var_export($_SESSION, true));
-}
-?>
+<div class="container-fluid">
+    <h1>Home</h1>
+    <?php
+    
+    if (is_logged_in(true)) {
+        //comment this out if you don't want to see the session variables
+        error_log("Session data: " . var_export($_SESSION, true));
+    }
+    
+    ?>
+</div>
 <?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
